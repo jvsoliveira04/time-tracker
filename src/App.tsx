13de9a5ba64528be45fef2ec18cbc8b2b5a56b05
jvsoliveira04 <jvs.oliveira04@gmail.com>
2026-09-09 11:03:48 -0300

    feat: create basic app component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Meu Ponto</h1>
+      <p>Aplicativo de controle de ponto</p>
+    </div>
+  )
+}
+
+export default App
